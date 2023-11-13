987dcf91273c460d06bcd91695d487dc2046454f
Alexandr Siryj <theobroma222@gmail.com>
2023-11-13 04:11:36 +0200

    react-icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { FaBeer } from 'react-icons/fa';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -10,6 +11,9 @@ function App() {
   return (
     <>
       <div>
+        <h3>
+          Lets go for a <FaBeer />?
+        </h3>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
