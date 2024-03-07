b44f23becb819dcd7a9efc74409f0c4918e69a0a
Alexandr Siryj <theobroma222@gmail.com>
2024-03-07 05:04:15 +0200

    add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 import { Forecast } from './components/forecast/forecast';
 import { GridExample } from './components/grid-example/grid-example';
 // import { StarterIntro } from './components/starter-intro/starter-intro';
 
 const App = () => (
-  <>
+  <QueryClientProvider client={queryClient}>
     {/* <StarterIntro /> */}
     <Forecast />
     <GridExample />
-  </>
+  </QueryClientProvider>
 );
 
 export default App;
