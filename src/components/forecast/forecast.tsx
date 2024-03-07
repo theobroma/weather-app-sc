99737a33b3642d79ad9052ff640af5130aa55048
Alexandr Siryj <theobroma222@gmail.com>
2024-03-07 05:47:16 +0200

    fetch

--- src/components/forecast/forecast.tsx
+++ src/components/forecast/forecast.tsx
@@ -1,10 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { loadForecast } from '../../api/forecast/forecast';
 import { FormRow } from '../form/common.styles';
 import { ForecastDay } from './forecast-day/forecast-day';
 
-export const Forecast = () => (
-  <FormRow sizes={3}>
-    <ForecastDay />
-    <ForecastDay />
-    <ForecastDay />
-  </FormRow>
-);
+export const Forecast = () => {
+  const { data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () => loadForecast(),
+  });
+
+  console.log('🚀 ~ Forecast ~ data:', data);
+
+  return (
+    <FormRow sizes={3}>
+      <ForecastDay />
+      <ForecastDay />
+      <ForecastDay />
+    </FormRow>
+  );
+};
