7e76e85a1f174c591189a34f61e86f86ffcb9fd7
Alexandr Siryj <theobroma222@gmail.com>
2024-03-31 23:58:29 +0300

    fix

--- src/components/current-weather/current-weather-location/current-weather-location.tsx
+++ src/components/current-weather/current-weather-location/current-weather-location.tsx
@@ -1,4 +1,4 @@
-import { LocationType } from '@api/forecast/forecast.interface';
+import { LocationType } from '@api/forecast/types/z.location';
 
 interface CurrentWeatherLocationProps {
   location: LocationType;
