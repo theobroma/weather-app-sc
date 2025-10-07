e9380cad3ad5a69c7d393599ea9f60391da6e43c
Alexandr Siryj <theobroma222@gmail.com>
2025-10-07 19:57:13 +0300

    sidebar styles

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,10 +1,29 @@
-import { Menu, MenuItem, Sidebar } from 'react-pro-sidebar';
+import { Menu, MenuItem, Sidebar, sidebarClasses } from 'react-pro-sidebar';
 import { NavLink, Outlet } from 'react-router-dom';
+
+import { ColorEnum } from '@/enums/color.enum';
 
 export const Layout = () => (
   <div style={{ display: 'flex', height: '100%', minHeight: '400px', backgroundColor: '#f0f0f0' }}>
-    <Sidebar>
-      <Menu>
+    <Sidebar
+      rootStyles={{
+        [`.${sidebarClasses.container}`]: {
+          backgroundColor: ColorEnum.Tertiary500,
+        },
+      }}
+    >
+      <Menu
+        menuItemStyles={{
+          button: {
+            // the active class will be added automatically by react router
+            // so we can use it to style the active menu item
+            [`&.active`]: {
+              backgroundColor: ColorEnum.Tertiary700,
+              color: ColorEnum.White,
+            },
+          },
+        }}
+      >
         <MenuItem component={<NavLink to="/"></NavLink>}>Weather</MenuItem>
         <MenuItem component={<NavLink to="/grid"></NavLink>}> Grid</MenuItem>
         <MenuItem component={<NavLink to="/starter"></NavLink>}>Starter</MenuItem>
